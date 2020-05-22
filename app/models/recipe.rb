48fa0db7532d71f7b1a759b1178af4440a5801ce
AssanSav <assane.savadogo81@gmail.com>
2020-05-22 13:15:22 -0400

    Recipes validations and association

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,14 @@
 class Recipe < ApplicationRecord
+    has_many :comments
+    belongs_to :user
+    belongs_to :category
+    has_many :recipe_ingredients, dependent: :destroy
+	has_many :ingredients, through: :recipe_ingredients
+
+    validates :name, presence: true, uniqueness: {:scope => [:user_id, :category_id]}
+    validates :image, format: {with: /\.(png|jpg)\Z/i,  message: 'must be a url for gif, jpg, or png image.'}
+    validates :directions, presence: true 
+    validate :ingredient_names
+
     
 end
