008ef8b9d1ebb782948c90f5c42479a05b33b0f5
AssanSav <assane.savadogo81@gmail.com>
2020-05-22 14:05:46 -0400

    Method to manipulate and set up logic for recipes creation from Api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Api.request["meals"].each do |attr|
+    # user = User.find_by(id: 1)
+    category = Category.find_or_create_by(id: attr["idMeal"], user_id: 2, name: attr["strCategory"])
+    recipe = Recipe.find_or_create_by(category_id: category.id, user_id: 2, name: attr["strMeal"], image: attr["strMealThumb"], directions: attr["strInstructions"])
+    20.times do |index|
+        if attr["strIngredient#{index + 1}"] !="" && attr["strMeasure#{index + 1}"] !=""
+            ingredient = Ingredient.find_or_create_by(name: attr["strIngredient#{index + 1}"]) 
+            recipe_ingredients = RecipeIngredient.create(recipe_id: recipe.id, ingredient_id: ingredient.id, amount: attr["strMeasure#{index + 1}"])
+        end
+    end
+end
