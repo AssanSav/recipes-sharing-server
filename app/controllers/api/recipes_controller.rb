895f613b5ac7a916537460637c0717ae66e5a8c4
AssanSav <assane.savadogo81@gmail.com>
2020-05-23 02:15:37 -0400

    Recipes controller create and show actions

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -3,9 +3,6 @@ class Api::RecipesController < ApplicationController
     def index 
         @recipes = Recipe.all
         if @recipes 
-            # options = {
-            #     include: [:category, :user]
-            # }
             render json: {
                 status: 200,
                 recipes: RecipeSerializer.new(@recipes)
@@ -16,5 +13,42 @@ class Api::RecipesController < ApplicationController
                 errors: "No Record found!"
             }
         end
+    end
+
+    def show 
+        @recipe = Recipe.find(params[:id])
+        if @recipe 
+            render json: {
+                status: 200,
+                recipe: RecipeSerializer.new(@recipe),
+                ingredients: @recipe.ingredients
+            }
+        else 
+            render json: {
+                status: 500,
+                errors: ["No recipes found!"]
+            }
+        end
+    end
+
+    def create 
+        @recipe = current_user.recipes.build(recipe_params)
+        if @recipe.save
+            render json: {
+                status: :created,
+                recipe: RecipeSerializer.new(@recipe)
+            }
+        else 
+            render json: {
+                status: 500,
+                errors: @recipe.errors.full_messages
+            }
+        end
+    end
+
+    private 
+    
+    def recipe_params
+        params.require(:recipe).permit(:name, :image, :directions, :category_id)
     end
 end
