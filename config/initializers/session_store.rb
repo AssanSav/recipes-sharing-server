fd12812f3c87e9a9848f515087348eb78495cb5d
AssanSav <assane.savadogo81@gmail.com>
2020-05-31 21:32:33 -0400

    Front end domain name added

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_recipes-sharing-app', domain: 'your-frontend-domain'
+  Rails.application.config.session_store :cookie_store, key: '_recipes-sharing-app', domain: 'https://recipes-sharing-client.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: '_recipes-sharing-app' 
 end
